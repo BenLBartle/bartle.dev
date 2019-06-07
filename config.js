dc08c68b27e4ddb306754f9a3382b6759df17501
Ben Bartle <ben.bartle@uk.ibm.com>
2019-06-07 20:30:21 +0100

    move blog posts

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
   title: 'Ben Bartle\'s Blog', // Required
-  author: 'bartle.dev', // Required
+  author: 'bartl.io', // Required
   description: 'Solution Architect, Developer & Pizza Maker',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://bartle.dev',
+    website: 'https://bartl.io',
     github: 'https://github.com/benlbartle',
     twitter: 'https://twitter.com/benbartle',
     linkedin: 'https://www.linkedin.com/in/benbartle/',
   },
   pathPrefix: '/',
-  siteUrl: 'https://bartle.dev/',
+  siteUrl: 'https://bartl.io/',
 };
